268f782035fa0738591baafa5ce29633d525cfac
zixun <chenyl.exe@gmail.com>
2017-02-23 14:58:25 +0800

    update project code

---
+++ Example/Pods/Target Support Files/Pods-AssistiveButton_Tests/Pods-AssistiveButton_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AssistiveButton_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AssistiveButton_Tests
+@end
---
+++ Example/Pods/Target Support Files/Pods-AssistiveButton_Tests/Pods-AssistiveButton_Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_AssistiveButton_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AssistiveButton_TestsVersionString[];
+
